935c52f23b8de9e3360a351e8362957be3fa4560
Aryan Agarwal <aryan@geekyants.com>
2019-06-30 04:57:38 +0530

    Redux logger installed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App/';
+import logger from 'redux-logger'
 import {Provider} from 'react-redux';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = {form: formReducer};
+const reducer = combineReducers(reducers);
+let store = createStore(reducer, applyMiddleware(logger));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
